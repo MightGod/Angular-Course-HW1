f0190955505d1c64e83635fccca356d2843b823f
uzieldavidi <uzieldavidi@gmail.com>
2018-08-08 22:33:14 +0300

    Finished HW1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HW1EX1Component } from './hw1-ex1/hw1-ex1.component';
+import { HW1EX2Component } from './hw1-ex2/hw1-ex2.component';
+import { HW1SELECTORComponent } from './hw1-selector/hw1-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HW1EX1Component,
+    HW1EX2Component,
+    HW1SELECTORComponent
   ],
   imports: [
     BrowserModule
